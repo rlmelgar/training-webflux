aaafa93c832650274deb3261c8e08171df0d139e
Raúl López <123734554+rlmelgar@users.noreply.github.com>
2024-10-05 12:00:38 +0200

    [RLM-TR-WEBFLUX-1] feat: basic exercises (#5)
    * [RLM-TR-WEBFLUX-1] feat :basic exercises
    
    * [RLM-TR-WEBFLUX-1] feat :basic exercises
    
    ---------
    
    Co-authored-by: raullme <raullme@ext.zara.com>

---
+++ src/main/java/com/kairosds/webflux/b_flux/Flux3ShowEvents.java
@@ -0,0 +1,42 @@
+package com.kairosds.webflux.b_flux;
+
+import java.util.List;
+
+import reactor.core.publisher.Flux;
+
+public class Flux3ShowEvents {
+
+  public static void main(String[] args) {
+    showEvents(List.of("Mario", "Luigi", "Peach"))
+        .subscribe(s -> System.out.println("Sus1:" + s));
+
+    try {
+      Thread.sleep(10000);
+    } catch (final InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static Flux<String> showEvents(List<String> stringList) {
+    return Flux.fromIterable(stringList)
+        .switchIfEmpty(Flux.error(NullPointerException::new))
+        .doOnSubscribe(subscription -> System.out.println("[SUBSCRIBE] P->S " + subscription))
+        .doFinally(signal -> System.out.println("[FINALLY] S->P " + signal))
+        .doOnNext(string -> System.out.println("[NEXT] P->S " + string))
+        .doOnError(NullPointerException.class, throwable -> System.out.println("[ERROR] P->S " + throwable))
+        .doOnError(throwable -> "Error numeric".equals(throwable.getMessage()),
+            throwable -> System.out.println("[ERROR] P->S " + throwable))
+        .doOnError(throwable -> {
+          System.out.println("[ERROR2] P->S " + throwable);
+        })
+        .doOnEach(signal -> System.out.println("[EACH] P->S " + signal))
+        .doFirst(() -> System.out.println("[FIRST] S->P"))
+        .doOnRequest(requestNumber -> System.out.println("[REQUEST] S->P " + requestNumber))
+        .doOnTerminate(() -> System.out.println("[TERMINATE] S->P"))
+        .doAfterTerminate(() -> System.out.println("[AFTER TERMINATE] S->P"))
+        .doOnCancel(() -> System.out.println("[CANCEL] S->P"))
+        .doOnComplete(() -> System.out.println("[COMPLETE] P->S"))
+        .doOnDiscard(String.class, string -> System.out.println("[DISCARD] P->S " + string));
+  }
+
+}
---
+++ src/main/java/com/kairosds/webflux/c_mono/Mono3ShowEvents.java
@@ -0,0 +1,55 @@
+package com.kairosds.webflux.c_mono;
+
+import java.util.logging.Level;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
+
+public class Mono3ShowEvents {
+
+  public static void main(String[] args) {
+    // execute("Mario Bros");
+    execute(null);
+
+    try {
+      Thread.sleep(10000);
+    } catch (final InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void execute(String value) {
+    final Mono<String> stringMono = showEvents(value);
+
+    stringMono.defaultIfEmpty("Nothing")
+        .subscribe(stringValue -> printResult("Final value: ", stringValue));
+  }
+
+  private static Mono<String> showEvents(String value) {
+    return Mono.justOrEmpty(value)
+        // .switchIfEmpty(Mono.error(RuntimeException::new))
+        .log("****************LOG*************", Level.WARNING, SignalType.ON_COMPLETE, SignalType.AFTER_TERMINATE)
+        .doOnSubscribe(subscription -> System.out.println("[SUBSCRIBE] P->S " + subscription))
+        .doOnNext(string -> System.out.println("[NEXT] P->S " + string))
+        .doOnError(throwable -> "Error numeric".equals(throwable.getMessage()),
+            throwable -> System.out.println("[ERROR] P->S " + throwable))
+        .doOnEach(signal -> System.out.println("[EACH] P->S " + signal))
+        .doFirst(() -> System.out.println("[FIRST] S->P"))
+        .doFinally(signal -> System.out.println("[FINALLY] S->P " + signal))
+        .doOnRequest(requestNumber -> System.out.println("[REQUEST] S->P " + requestNumber))
+        .doOnTerminate(() -> System.out.println("[TERMINATE] S->P"))
+        .doAfterTerminate(() -> System.out.println("[AFTER TERMINATE] S->P"))
+        .doOnCancel(() -> System.out.println("[CANCEL] S->P"))
+        .doOnSuccess(string -> System.out.println("[SUCCESS] P->S " + string))
+        .doOnDiscard(String.class, string -> System.out.println("[DISCARD] P->S " + string));
+  }
+
+  private static void printResult(String subscriber, String stringValue) {
+    System.out.println();
+    System.out.println("*****************");
+    System.out.println("********** (" + subscriber + ")Value: " + stringValue);
+    System.out.println("*****************");
+    System.out.println();
+  }
+
+}
